c58e149455a88d2ba59200641b9a7bc0ebbff5f2
requestly-team <35186614+requestly-team@users.noreply.github.com>
2022-05-06 02:04:45 +0530

    Updated env

--- utils/firebase.js
+++ utils/firebase.js
@@ -10,7 +10,7 @@ export const initializeFirebase = () => {
       authDomain: process.env.AUTH_DOMAIN,
       databaseURL: process.env.DATABASE_URL,
       projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKER,
+      storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
     });
   }
