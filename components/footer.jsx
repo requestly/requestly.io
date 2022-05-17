c65dfb604756fedd12d92fd532429cacb603e628
Shrey Gupta <shrey.iiitmg@gmail.com>
2022-05-17 15:59:24 +0530

    Intercom integrated in landing pages

--- components/footer.jsx
+++ components/footer.jsx
@@ -16,15 +16,43 @@ import {
 
 const Footer = ({ soft }) => {
   useEffect(() => {
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = "1c7370cc-6ff1-446f-89fa-9769ac56b756";
+    window.intercomSettings = {
+      api_base: "https://api-iam.intercom.io",
+      app_id: "otke9j11",
+    };
 
     (function () {
-      var d = document;
-      var s = d.createElement("script");
-      s.src = "https://client.crisp.chat/l.js";
-      s.async = 1;
-      d.getElementsByTagName("head")[0].appendChild(s);
+      var w = window;
+      var ic = w.Intercom;
+      if (typeof ic === "function") {
+        ic("reattach_activator");
+        ic("update", w.intercomSettings);
+      } else {
+        var d = document;
+        var i = function () {
+          i.c(arguments);
+        };
+        i.q = [];
+        i.c = function (args) {
+          i.q.push(args);
+        };
+        w.Intercom = i;
+        var l = function () {
+          var s = d.createElement("script");
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://widget.intercom.io/widget/otke9j11";
+          var x = d.getElementsByTagName("script")[0];
+          x.parentNode.insertBefore(s, x);
+        };
+        if (document.readyState === "complete") {
+          l();
+        } else if (w.attachEvent) {
+          w.attachEvent("onload", l);
+        } else {
+          w.addEventListener("load", l, false);
+        }
+      }
     })();
   }, []);
 
