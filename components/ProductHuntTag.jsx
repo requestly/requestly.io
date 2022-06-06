5f55bf18e44c17d89c0ab9609f0461df9f204830
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2022-06-06 07:13:46 +0530

    [RQLY-54] fix: using img instead of next/image to html img tag

--- components/ProductHuntTag.jsx
+++ components/ProductHuntTag.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -10,9 +9,9 @@ const ProductHuntTag = () => {
         rel="noreferrer"
         passHref
       >
-        <Image 
+        <img
           src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=343176&theme=light&period=daily" alt="Requestly&#0032;for&#0032;Android - A&#0032;powerful&#0032;open&#0045;source&#0032;debugger&#0032;for&#0032;Android&#0032;apps | Product Hunt" 
-          style="width: 250px; height: 54px;" 
+          style={{width: "250px", height: "54px"}}
           width="250" 
           height="54" 
         />
